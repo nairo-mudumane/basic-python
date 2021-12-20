6cfa6728b63efea2cfc67182f65330cbd1f2e33a
nairoMudumane <nazirmudumane@gmail.com>
2021-12-20 15:33:26 +0200

    conjuntos e tuplas

---
+++ estrutura-de-dados.py
@@ -0,0 +1,13 @@
+# as tuplas sao imutaveis, nao se adciona nem remove-se valores.
+my_tuple = ('Gui','Magui')
+# print(my_tuple)
+
+# dicionarios
+my_dict = { 'name': 'Nairo Mudumane', 'age': 21 }
+# print(type(my_dict))
+
+# conjuntos
+# no conjunto nao podemos ter items repetidos. No conjunto nao existe ordem, nao tem indice
+my_set = {'Gui','Lau'}
+# print(my_set)
+# print(type(my_set))
