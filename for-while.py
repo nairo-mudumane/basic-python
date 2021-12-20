c136708f9a511875d11733c4131b9cea1d577916
nairoMudumane <nazirmudumane@gmail.com>
2021-12-20 15:07:04 +0200

    loop

---
+++ for-while.py
@@ -0,0 +1,20 @@
+# names = ['Nairo', 'Guilherme', 'Marcelo', 'Julia', 'Joao']
+
+# for name in names:
+#     print(name)
+
+# numbs = range(5)
+# numbs = range(5, 10)
+# numbs = range(0, 100, 2)
+# for i in numbs:
+#     print(i)
+
+# for i in range(0, 10, 2):
+#     if i != 0:
+#         print(i)
+
+# names = ['Nairo', 'Guilherme', 'Marcelo', 'Julia', 'Joao']
+# for i in range(len(names)):
+#     print(names[i])
+
+# podemos usar o for dentro de uma string
