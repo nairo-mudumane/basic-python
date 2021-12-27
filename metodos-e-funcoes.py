9648c841e01ac75f2b3d6f1c4714827f752a6f6d
nairoMudumane <nazirmudumane@gmail.com>
2021-12-27 06:19:34 +0200

    methods and functions

---
+++ metodos-e-funcoes.py
@@ -0,0 +1,12 @@
+# def soma(num1,numb2):
+#     return num1 + numb2
+
+# print(soma(12.36, 75.60))
+
+def lenght(num):
+    if  len(str(num)) == 2:
+        return True
+    else:
+        return False
+
+print(lenght(22))
